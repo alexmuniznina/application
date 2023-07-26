0b562652dc0697940b89978a4ccdde01f319b44f
Alex Nina <alexnina@Alexs-MacBook-Pro.local>
2023-07-26 14:17:30 -0400

    home-component

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { MainRoutingModule } from './main-routing/main-routing.module';
+import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main/main.component';
 import { HomeComponent } from './home/home.component';
 import { FavoritosComponent } from './favoritos/favoritos.component';
@@ -18,5 +18,6 @@ import { AlertasComponent } from './alertas/alertas.component';
     AlertasComponent,
   ],
   imports: [SharedModule, MainRoutingModule],
+  exports: [MainComponent],
 })
 export class MainModule {}
--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
 
-const MaterialComponents = [MatToolbarModule, MatIconModule];
+const MaterialComponents = [
+  MatToolbarModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonToggleModule,
+  MatButtonModule,
+];
 
 @NgModule({
   declarations: [],
